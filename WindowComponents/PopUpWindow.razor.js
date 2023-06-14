27b1027288fb357e8d0ec1f65c4d8b2bff167752
hjc2000 <hjc.666@foxmail.com>
2023-06-14 10:29:09 +0800

    导入输出设置

--- WindowComponents/PopUpWindow.razor.js
+++
@@ -1,8 +0,0 @@
-﻿export function AddWindowEventListener(event, dotnetObj, methodName)
-{
-	window.addEventListener(event, (e) =>
-	{
-		e.preventDefault();
-		dotnetObj.invokeMethodAsync(methodName, e.pageX, e.pageY);
-	});
-}
