553f5b963f9553f18f0357aba9b2f87839901ea0
hjc2000 <huangjincanqqmail@qq.com>
2023-08-09 11:14:02 +0800

    重命名

--- WindowComponents/Dialog.razor.cs
+++ WindowComponents/Dialog.razor.cs
@@ -9,7 +9,7 @@ public partial class Dialog : IAsyncDisposable
 	#region 生命周期
 	public Dialog()
 	{
-		_oncloseCallbackHelper.CallbackAction = async () =>
+		_onclose_callback_helper.CallbackAction = async () =>
 		{
 			Console.WriteLine("弹窗关闭");
 			await DialogClosed.InvokeAsync();
@@ -26,7 +26,7 @@ public partial class Dialog : IAsyncDisposable
 		if (firstRender)
 		{
 			_jsm = new JSModule(_jsrt, "./_content/WindowComponents/Dialog.razor.js");
-			_dialogWrapper = await _jsm.InvokeAsync<IJSObjectReference>("DialogWrapper.create", _dialogElement, _oncloseCallbackHelper.DotNetHelper);
+			_dialogWrapper = await _jsm.InvokeAsync<IJSObjectReference>("DialogWrapper.create", _dialogElement, _onclose_callback_helper.DotNetHelper);
 			_initTcs.SetResult();
 		}
 	}
@@ -45,7 +45,7 @@ public partial class Dialog : IAsyncDisposable
 		Console.WriteLine("Dialog 释放");
 		await _jsm.DisposeAsync();
 		await _dialogWrapper.DisposeAsync();
-		_oncloseCallbackHelper.Dispose();
+		_onclose_callback_helper.Dispose();
 	}
 	#endregion
 
@@ -53,7 +53,7 @@ public partial class Dialog : IAsyncDisposable
 	private IJSObjectReference _dialogWrapper = default!;
 	private TaskCompletionSource _initTcs = new();
 	private ElementReference _dialogElement;
-	private CallbackHelper _oncloseCallbackHelper = new();
+	private CallbackHelper _onclose_callback_helper = new();
 
 	[Parameter]
 	public EventCallback DialogClosed { get; set; }
@@ -65,7 +65,7 @@ public partial class Dialog : IAsyncDisposable
 	/// 显示对话框
 	/// </summary>
 	/// <returns></returns>
-	public async Task ShowModal()
+	public async Task ShowModalAsync()
 	{
 		await _initTcs.Task;
 		await _dialogWrapper.InvokeVoidAsync("showModal", _dialogElement);
@@ -75,13 +75,13 @@ public partial class Dialog : IAsyncDisposable
 	/// 关闭对话框
 	/// </summary>
 	/// <returns></returns>
-	public async Task Close()
+	public async Task CloseAsync()
 	{
 		await _initTcs.Task;
 		await _dialogWrapper.InvokeVoidAsync("close", _dialogElement);
 	}
 
-	public async Task<bool> IsOpen()
+	public async Task<bool> IsOpened()
 	{
 		await _initTcs.Task;
 		return await _dialogWrapper.InvokeAsync<bool>("isOpened");
